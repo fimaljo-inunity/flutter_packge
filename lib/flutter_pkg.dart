985bff1bd8db5ba568b8558a9d89e8a034d24a9a
InUnity <inunity@InUnitys-Mini.mshome.net>
2023-02-03 17:51:37 +0530

    new commit

---
+++ lib/flutter_pkg.dart
@@ -0,0 +1,48 @@
+library flutter_pkg;
+
+import 'package:flutter/material.dart';
+
+// class CustomButton extends StatelessWidget {
+//   var onPressed;
+//   final Widget child;
+//   var style;
+//   CustomButton({Key key, required this.onPressed, this.child, this.style})
+//       : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return TextButton(
+//         onPressed: onPressed,
+//         style: TextButton.styleFrom(
+//           padding: const EdgeInsets.all(16.0),
+//           primary: Colors.white,
+//           backgroundColor: Colors.blue,
+//           elevation: 9.0,
+//           textStyle: const TextStyle(
+//             fontSize: 20,
+//           ),
+//         ),
+//         child: child);
+//   }
+// }
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key, required this.child, this.onPres, this.styl});
+  final onPres;
+  final Widget child;
+  final styl;
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: onPres,
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.all(16.0),
+          primary: Colors.white,
+          backgroundColor: Colors.blue,
+          elevation: 9.0,
+          textStyle: const TextStyle(
+            fontSize: 20,
+          ),
+        ),
+        child: child);
+  }
+}
